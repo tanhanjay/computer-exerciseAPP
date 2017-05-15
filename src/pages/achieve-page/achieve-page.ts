9ef7059dfd7e8222c5537d9686e33cd6ced706a3
tanhanjay <1160343785@qq.com>
2017-05-15 13:46:23 +0800

    分享

--- src/pages/achieve-page/achieve-page.ts
+++ src/pages/achieve-page/achieve-page.ts
@@ -1,4 +1,4 @@
-import { UserInfoService,UserInfo } from './../../providers/user-info.service';
+import { UserInfoService, UserInfo } from './../../providers/user-info.service';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 import { SocialSharing } from '@ionic-native/social-sharing';
@@ -16,27 +16,39 @@ import { SocialSharing } from '@ionic-native/social-sharing';
 })
 export class AchievePage {
 
-  userInfo:UserInfo;
-  constructor(public navCtrl: NavController, public navParams: NavParams,private userInfoService: UserInfoService,private socialSharing: SocialSharing,private alertCtrl:AlertController) {
+  userInfo: UserInfo;
+  constructor(public navCtrl: NavController, public navParams: NavParams, private userInfoService: UserInfoService, private socialSharing: SocialSharing, private alertCtrl: AlertController) {
   }
- share(){
-   this.socialSharing.canShareVia("QQ").then((value)=>{
-     this.alertCtrl.create({
-       title:"能分享到QQ"
-     }).present();
-   }).catch((value)=>{
-     this.alertCtrl.create({
-       title:"不能分享到QQ"
-     }).present();
-   })
-    this.socialSharing.share("哈哈哈哈").then((data)=>{
-      this.alertCtrl.create({
-        title:"成功"
-      }).present();
-    },(data)=>{
-      this.alertCtrl.create({
-        title:"失败"
-      }).present();
+  share() {
+    //  this.socialSharing.canShareVia("QQ").then((value)=>{
+    //    this.alertCtrl.create({
+    //      title:"能分享到QQ"
+    //    }).present();
+    //  }).catch((value)=>{
+    //    this.alertCtrl.create({
+    //      title:"不能分享到QQ"
+    //    }).present();
+    //  })
+    //   this.socialSharing.share("哈哈哈哈").then((data)=>{
+    //     this.alertCtrl.create({
+    //       title:"成功"
+    //     }).present();
+    //   },(data)=>{
+    //     this.alertCtrl.create({
+    //       title:"失败"
+    //     }).present();
+    //   });
+    this.socialSharing.canShareViaEmail().then(() => {
+      // Sharing via email is possible
+    }).catch(() => {
+      // Sharing via email is not possible
+    });
+
+    // Share via email
+    this.socialSharing.shareViaEmail('Body', 'Subject', ['recipient@example.org']).then(() => {
+      // Success!
+    }).catch(() => {
+      // Error!
     });
   }
   ionViewDidLoad() {
